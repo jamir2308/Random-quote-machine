e6b830d871134cc816ead253f51775f41bcd31d5
jamir.patiño <jamir2308@gmail.com>
2020-08-27 12:56:46 -0500

    Updates

---
+++ precache-manifest.ea0e72021244aae97e8ac7095bfa9954.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c451ef37789546c3ce4b2356ae0cace6",
+    "url": "/imc-comfenalco/index.html"
+  },
+  {
+    "revision": "71066c12bc9e44b0b8a3",
+    "url": "/imc-comfenalco/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e6c3aba8de11282b1382",
+    "url": "/imc-comfenalco/static/js/2.09d32d23.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/imc-comfenalco/static/js/2.09d32d23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71066c12bc9e44b0b8a3",
+    "url": "/imc-comfenalco/static/js/main.02402c54.chunk.js"
+  },
+  {
+    "revision": "cf5ef7bb299543477c49",
+    "url": "/imc-comfenalco/static/js/runtime-main.e4874b84.js"
+  }
+]);
